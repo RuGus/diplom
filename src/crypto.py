af3b88985c1dbaaf79f50d4f2605c5b582455593
Ruslan Gusmanov <rgusmanov@eqvantalab.ru>
2023-11-03 16:02:46 +0300

    Added some drafts

---
+++ src/crypto.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+def sign_file():
+    pass
+
+def encrypt_file():
+    pass
+
+def decrypt_file():
+    pass
+
+def unsign_file():
+    pass
+
+def pack_file():
+    pass
+
+def unpack_file():
+    pass
