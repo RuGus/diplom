84354f75b12f61da4fff5398daabc384c79320a5
Ruslan Gusmanov <rgusmanov@eqvantalab.ru>
2023-11-11 17:35:49 +0300

    Added file creator for demo

---
+++ scripts/file_creator.py
@@ -0,0 +1,26 @@
+import os
+from minio import Minio
+
+MINIO_ROOT_USER = os.environ.get("MINIO_ROOT_USER")
+MINIO_ROOT_PASSWORD = os.environ.get("MINIO_ROOT_PASSWORD")
+MINIO_HOST = os.environ.get("MINIO_HOST", "minio")
+MINIO_PORT = int(os.environ.get("MINIO_PORT", 9000))
+BUCKET = "new-bucket"
+
+s3_client = Minio(
+    f"{MINIO_HOST}:{MINIO_PORT}",
+    access_key=MINIO_ROOT_USER,
+    secret_key=MINIO_ROOT_PASSWORD,
+    secure=False,
+)
+if not s3_client.bucket_exists(BUCKET):
+    s3_client.make_bucket(BUCKET)
+
+for i in range(10):
+    file_name = f"{i}.txt"
+    with open(file_name, "w") as file:
+        file.write(str(i))
+    s3_client.fput_object(
+        bucket_name=BUCKET, object_name=file_name, file_path=file_name
+    )
+    os.remove(file_name)
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ./scripts/wait-for-it.sh rabbit_node:5672
 ./scripts/wait-for-it.sh minio:9000
+python scripts/file_creator.py
 sleep 10
 python src/main.py
